4dad438c3a87455bdb95287bd948b4c05eccc571
Luca Barone <baro.luc@gmail.com>
2026-03-02 16:59:34 +0100

    feat: add makeAsymmetricTicks and makeAsymmetricScale functions

---
+++ src/makeAsymmetricTicks.ts
@@ -0,0 +1,10 @@
+export function makeAsymmetricTicks(args: {
+  positivePercentage: number;
+  values: readonly number[];
+  // maxPositive: number;
+  // maxNegative: number;
+}): number[] {
+  return [];
+}
+
+export function makeAsymmetricScale() {}
